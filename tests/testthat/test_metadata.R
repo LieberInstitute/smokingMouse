53fc771dc0cbdc40dc7ff52cb062f34091457a78
daianna21 <daianna2121@outlook.es>
2023-03-08 10:01:46 -0600

    Add pkg name

--- tests/testthat/test_metadata.R
+++ tests/testthat/test_metadata.R
@@ -5,7 +5,7 @@ test_that("metadata is valid",
     if(!requireNamespace("AnnotationHubData", quietly = TRUE))
         BiocManager::install("AnnotationHubData")
 
-    path <- find.package("")
+    path <- find.package("smokingMouse")
     metadata <- system.file("extdata", "metadata.csv", package = "smokingMouse")
     expect_true(AnnotationHubData::makeAnnotationHubMetadata(path, metadata))
 })
