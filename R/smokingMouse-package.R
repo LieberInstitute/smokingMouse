803658692a48112936a9fd8a7aaf25f1a02c4198
daianna21 <daianna2121@outlook.es>
2023-03-08 19:44:16 -0600

    Add description to smokingMouse-package.R file

--- R/smokingMouse-package.R
+++ R/smokingMouse-package.R
@@ -1,8 +1,8 @@
-#' Provides access to raw and processed data of the smokingMouse project
+#' Provides access to data of the smokingMouse project
 #'
-#' @description This is an ExperimentHub package that provides access to the raw and processed data generated at the gene, exon, tx and jx levels,
-#' in all phases of the smoking mouse project. See https://github.com/LieberInstitute/smokingMouse_Indirects.
-#'
+#' @description This is an ExperimentHub package that provides access to the data at the gene, exon, transcript and junction levels,
+#' used in the analyses of the smokingMouse project. See https://github.com/LieberInstitute/smokingMouse_Indirects.
+#' License: Artistic-2.0
 #' @name smokingMouse-package
 #' @aliases smokingMouse-package smokingMouse
 #' @docType package
