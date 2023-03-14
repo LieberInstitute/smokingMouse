f9b45288aa875a7401fe20b85622a26ce94a79e3
daianna21 <daianna2121@outlook.es>
2023-03-13 22:14:08 -0600

    Add full path to AnnotationHubData::makeAnnotationHubMetadata(pathToPackage, fileName) instead of using find.package or system.file to avoid error

--- tests/testthat/test_metadata.R
+++ tests/testthat/test_metadata.R
@@ -2,7 +2,5 @@ context("metadata validity")
 
 test_that("metadata is valid",
 {
-    path <- find.package("smokingMouse")
-    metadata <- "metadata.csv"
-    expect_type(AnnotationHubData::makeAnnotationHubMetadata(path, metadata), "list")
+    expect_type(AnnotationHubData::makeAnnotationHubMetadata(pathToPackage = "/Users/daiannagonzalez/Desktop/smokingMouse" , "metadata.csv"), "list")
 })
