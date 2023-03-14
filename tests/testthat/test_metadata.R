49ccf5eee0f3e28aea8437d7e299936f93a923d9
daianna21 <daianna2121@outlook.es>
2023-03-13 21:05:37 -0600

    Add "AnnotationHubData" to Imports in DESCRIPTION file to import that function

--- tests/testthat/test_metadata.R
+++ tests/testthat/test_metadata.R
@@ -2,9 +2,6 @@ context("metadata validity")
 
 test_that("metadata is valid",
 {
-    if(!requireNamespace("AnnotationHubData", quietly = TRUE))
-        BiocManager::install("AnnotationHubData")
-
     path <- find.package("smokingMouse")
     metadata <- "metadata.csv"
     expect_type(AnnotationHubData::makeAnnotationHubMetadata(path, metadata), "list")
