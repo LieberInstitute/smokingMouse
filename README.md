05dd38f3851b3640cee4f82f364ec90c9d87f89b
daianna21 <daianna2121@outlook.es>
2023-03-17 12:36:29 -0600

    ran biocthis::use_bioc_pkg_templates()

---
+++ README.md
@@ -0,0 +1,166 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# smokingMouse
+
+<!-- badges: start -->
+
+[![GitHub
+issues](https://img.shields.io/github/issues/LieberInstitute/smokingMouse)](https://github.com/LieberInstitute/smokingMouse/issues)
+[![GitHub
+pulls](https://img.shields.io/github/issues-pr/LieberInstitute/smokingMouse)](https://github.com/LieberInstitute/smokingMouse/pulls)
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+[![Bioc release
+status](http://www.bioconductor.org/shields/build/release/data-experiment/smokingMouse.svg)](https://bioconductor.org/checkResults/release/data-experiment-LATEST/smokingMouse)
+[![Bioc devel
+status](http://www.bioconductor.org/shields/build/devel/data-experiment/smokingMouse.svg)](https://bioconductor.org/checkResults/devel/data-experiment-LATEST/smokingMouse)
+[![Bioc downloads
+rank](https://bioconductor.org/shields/downloads/release/smokingMouse.svg)](http://bioconductor.org/packages/stats/bioc/smokingMouse/)
+[![Bioc
+support](https://bioconductor.org/shields/posts/smokingMouse.svg)](https://support.bioconductor.org/tag/smokingMouse)
+[![Bioc last
+commit](https://bioconductor.org/shields/lastcommit/devel/data-experiment/smokingMouse.svg)](http://bioconductor.org/checkResults/devel/data-experiment-LATEST/smokingMouse/)
+[![Bioc
+dependencies](https://bioconductor.org/shields/dependencies/release/smokingMouse.svg)](https://bioconductor.org/packages/release/bioc/html/smokingMouse.html#since)
+[![R-CMD-check-bioc](https://github.com/LieberInstitute/smokingMouse/actions/workflows/R-CMD-check-bioc.yaml/badge.svg)](https://github.com/LieberInstitute/smokingMouse/actions/workflows/R-CMD-check-bioc.yaml)
+<!-- badges: end -->
+
+The goal of `smokingMouse` is to …
+
+## Installation instructions
+
+Get the latest stable `R` release from
+[CRAN](http://cran.r-project.org/). Then install `smokingMouse` from
+[Bioconductor](http://bioconductor.org/) using the following code:
+
+``` r
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+}
+
+BiocManager::install("smokingMouse")
+```
+
+And the development version from
+[GitHub](https://github.com/LieberInstitute/smokingMouse) with:
+
+``` r
+BiocManager::install("LieberInstitute/smokingMouse")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library("smokingMouse")
+## basic example code
+```
+
+What is special about using `README.Rmd` instead of just `README.md`?
+You can include R chunks like so:
+
+``` r
+summary(cars)
+#>      speed           dist       
+#>  Min.   : 4.0   Min.   :  2.00  
+#>  1st Qu.:12.0   1st Qu.: 26.00  
+#>  Median :15.0   Median : 36.00  
+#>  Mean   :15.4   Mean   : 42.98  
+#>  3rd Qu.:19.0   3rd Qu.: 56.00  
+#>  Max.   :25.0   Max.   :120.00
+```
+
+You’ll still need to render `README.Rmd` regularly, to keep `README.md`
+up-to-date.
+
+You can also embed plots, for example:
+
+In that case, don’t forget to commit and push the resulting figure
+files, so they display on GitHub!
+
+## Citation
+
+Below is the citation output from using `citation('smokingMouse')` in R.
+Please run this yourself to check for any updates on how to cite
+**smokingMouse**.
+
+``` r
+print(citation('smokingMouse'), bibtex = TRUE)
+#> 
+#> To cite package 'smokingMouse' in publications use:
+#> 
+#>   Gonzalez-Padilla D, Collado-Torres L (2023). _Provides access to
+#>   smokingMouse project data _. doi:10.18129/B9.bioc.smokingMouse
+#>   <https://doi.org/10.18129/B9.bioc.smokingMouse>,
+#>   https://github.com/LieberInstitute/smokingMouse/smokingMouse - R
+#>   package version 0.99.0,
+#>   <http://www.bioconductor.org/packages/smokingMouse>.
+#> 
+#> A BibTeX entry for LaTeX users is
+#> 
+#>   @Manual{,
+#>     title = {Provides access to smokingMouse project data },
+#>     author = {Daianna Gonzalez-Padilla and Leonardo Collado-Torres},
+#>     year = {2023},
+#>     url = {http://www.bioconductor.org/packages/smokingMouse},
+#>     note = {https://github.com/LieberInstitute/smokingMouse/smokingMouse - R package version 0.99.0},
+#>     doi = {10.18129/B9.bioc.smokingMouse},
+#>   }
+#> 
+#>   Gonzalez-Padilla D, Collado-Torres L (2023). "Provides access to
+#>   smokingMouse project data." _bioRxiv_. doi:10.1101/TODO
+#>   <https://doi.org/10.1101/TODO>,
+#>   <https://www.biorxiv.org/content/10.1101/TODO>.
+#> 
+#> A BibTeX entry for LaTeX users is
+#> 
+#>   @Article{,
+#>     title = {Provides access to smokingMouse project data},
+#>     author = {Daianna Gonzalez-Padilla and Leonardo Collado-Torres},
+#>     year = {2023},
+#>     journal = {bioRxiv},
+#>     doi = {10.1101/TODO},
+#>     url = {https://www.biorxiv.org/content/10.1101/TODO},
+#>   }
+```
+
+Please note that the `smokingMouse` was only made possible thanks to
+many other R and bioinformatics software authors, which are cited either
+in the vignettes and/or the paper(s) describing this package.
+
+## Code of Conduct
+
+Please note that the `smokingMouse` project is released with a
+[Contributor Code of
+Conduct](http://bioconductor.org/about/code-of-conduct/). By
+contributing to this project, you agree to abide by its terms.
+
+## Development tools
+
+- Continuous code testing is possible thanks to [GitHub
+  actions](https://www.tidyverse.org/blog/2020/04/usethis-1-6-0/)
+  through *[usethis](https://CRAN.R-project.org/package=usethis)*,
+  *[remotes](https://CRAN.R-project.org/package=remotes)*, and
+  *[rcmdcheck](https://CRAN.R-project.org/package=rcmdcheck)* customized
+  to use [Bioconductor’s docker
+  containers](https://www.bioconductor.org/help/docker/) and
+  *[BiocCheck](https://bioconductor.org/packages/3.16/BiocCheck)*.
+- Code coverage assessment is possible thanks to
+  [codecov](https://codecov.io/gh) and
+  *[covr](https://CRAN.R-project.org/package=covr)*.
+- The [documentation
+  website](http://LieberInstitute.github.io/smokingMouse) is
+  automatically updated thanks to
+  *[pkgdown](https://CRAN.R-project.org/package=pkgdown)*.
+- The code is styled automatically thanks to
+  *[styler](https://CRAN.R-project.org/package=styler)*.
+- The documentation is formatted thanks to
+  *[devtools](https://CRAN.R-project.org/package=devtools)* and
+  *[roxygen2](https://CRAN.R-project.org/package=roxygen2)*.
+
+For more details, check the `dev` directory.
+
+This package was developed using
+*[biocthis](https://bioconductor.org/packages/3.16/biocthis)*.
--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,10 +1,250 @@
 
-## This was run locally but can be ran on JHPCE at. ....????
+## This was run locally but can be run on JHPCE at (path a smokingMouse_Indirects).... ?
+## See https://github.com/LieberInstitute/smokingMouse_Indirects for the analysis code of the project
+
 
 library(here)
 library(sessioninfo)
 library(SummarizedExperiment)
 library(GenomicRanges)
+
+
+## Load original rse objects (from the smokingMouse_Indirects project) with the correct sample info in colData and logcounts as an assay:
+
+## Gene rse contains QC stats from addPerCellQC() in colData as well
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_gene_logcounts.Rdata"), verbose = TRUE)
+# rse_gene
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_exon_logcounts.Rdata"), verbose = TRUE)
+# rse_exon
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_jx_logcounts.Rdata"), verbose = TRUE)
+# rse_jx
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_tx_logcounts.Rdata"), verbose = TRUE)
+# rse_tx
+## Human data
+load(here("~/Desktop/smokingMouse_Indirects/raw-data/Genes_DE_sva.rda"), verbose = TRUE)
+# fetalGene
+# adultGene
+
+
+## Create the complete RSE objects at gene, exon, jxn and tx level adding info of which features and samples passed
+## filtering steps and which features were DE in the different groups of samples.
+
+## The following analyses were done here: https://github.com/LieberInstitute/smokingMouse_Indirects
+####################################
+##    02_build_objects analyses
+####################################
+
+## Analyses:
+
+## 1. Feature filtering
+
+## rse objects with retained features after feature filtering
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_gene_filt.Rdata"), verbose = TRUE)
+# rse_gene_filt
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_exon_filt.Rdata"), verbose = TRUE)
+# rse_exon_filt
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_jx_filt.Rdata"), verbose = TRUE)
+# rse_jx_filt
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/02_build_objects/rse_tx_filt.Rdata"), verbose = TRUE)
+# rse_tx_filt
+
+## Add column to rowData (of the original rse objects) with the info of the features that were retained and dropped after feature filtering
+rowData(rse_gene)$retained_after_feature_filtering <- unlist(sapply(rowData(rse_gene)$gencodeID, function(x){if(x %in% rowData(rse_gene_filt)$gencodeID){TRUE} else {FALSE}}))
+rowData(rse_exon)$retained_after_feature_filtering <- unlist(sapply(rowData(rse_exon)$exon_gencodeID, function(x){if(x %in% rowData(rse_exon_filt)$exon_gencodeID){TRUE} else {FALSE}}))
+rowData(rse_jx)$retained_after_feature_filtering <- unlist(sapply(rownames(rowData(rse_jx)), function(x){if(x %in% rownames(rowData(rse_jx_filt))){TRUE} else {FALSE}}))
+rowData(rse_tx)$retained_after_feature_filtering <- unlist(sapply(rowData(rse_tx)$transcript_id, function(x){if(x %in% rowData(rse_tx_filt)$transcript_id){TRUE} else {FALSE}}))
+
+
+
+#############################
+##    03_EDA analyses
+#############################
+
+########### 02_QC ###########
+
+## 1. Sample filtering by QC metrics
+
+## rse objects with samples that passed the QC filtering
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/03_EDA/02_QC/rse_gene_blood_qc.Rdata"), verbose = TRUE)
+# rse_gene_blood_qc
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/03_EDA/02_QC/rse_gene_brain_pups_qc.Rdata"), verbose = TRUE)
+# rse_gene_brain_pups_qc
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/03_EDA/02_QC/rse_gene_brain_adults_qc.Rdata"), verbose = TRUE)
+# rse_gene_brain_adults_qc
+
+## Add column to colData (of the original rse objects) with the info of samples retained and dropped after sample filtering by QC
+retained_samples <- union(rse_gene_blood_qc$SAMPLE_ID, union(rse_gene_brain_pups_qc$SAMPLE_ID, rse_gene_brain_adults_qc$SAMPLE_ID))
+colData(rse_gene)$retained_after_QC_sample_filtering <- unlist(sapply(rse_gene$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+colData(rse_exon)$retained_after_QC_sample_filtering <- unlist(sapply(rse_exon$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+colData(rse_tx)$retained_after_QC_sample_filtering <- unlist(sapply(rse_tx$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+colData(rse_jx)$retained_after_QC_sample_filtering <- unlist(sapply(rse_jx$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+
+
+
+########### 03_PCA_MDS ###########
+
+## 1. Manual sample filtering (removal of rare samples identified in PCA plots)
+
+## rse objects with samples that passed QC and manual filtering
+## (No blood sample was manually removed)
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/03_EDA/03_PCA/rse_gene_brain_adults_qc_afterPCA.Rdata"), verbose = TRUE)
+# rse_gene_brain_adults_qc_afterPCA
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/03_EDA/03_PCA/rse_gene_brain_pups_qc_afterPCA.Rdata"), verbose = TRUE)
+# rse_gene_brain_pups_qc_afterPCA
+
+## Add column to colData (of the original rse objects) with the info of samples retained and dropped after manual sample filtering
+retained_samples <- union(rse_gene_blood_qc$SAMPLE_ID, union(rse_gene_brain_adults_qc_afterPCA$SAMPLE_ID, rse_gene_brain_pups_qc_afterPCA$SAMPLE_ID))
+colData(rse_gene)$retained_after_manual_sample_filtering <- unlist(sapply(rse_gene$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+colData(rse_exon)$retained_after_manual_sample_filtering <- unlist(sapply(rse_exon$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+colData(rse_tx)$retained_after_manual_sample_filtering <- unlist(sapply(rse_tx$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+colData(rse_jx)$retained_after_manual_sample_filtering <- unlist(sapply(rse_jx$SAMPLE_ID, function(x){if(x %in% retained_samples){TRUE} else {FALSE}}))
+
+
+
+#############################
+##    04_DEA analyses
+#############################
+
+## 1. Differential expression analyses (with fitted models only)
+
+########### Gene level ###########
+
+## Data frames with DEGs
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Gene_analysis/de_genes_pups_nicotine_fitted.Rdata"), verbose = TRUE)
+# de_genes_pups_nicotine_fitted
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Gene_analysis/de_genes_pups_smoking_fitted.Rdata"), verbose = TRUE)
+# de_genes_pups_smoking_fitted
+
+
+## Add info of DEGs to rowData of rse objects
+
+## No DEGs in adult blood samples
+## Smoking
+rowData(rse_gene)$DE_in_adult_blood_smoking <- rep(FALSE, dim(rse_gene)[1])
+
+## No DEGs in adult brain samples
+## Nicotine
+rowData(rse_gene)$DE_in_adult_brain_nicotine <- rep(FALSE, dim(rse_gene)[1])
+## Smoking
+rowData(rse_gene)$DE_in_adult_brain_smoking <- rep(FALSE, dim(rse_gene)[1])
+
+## DEGs in pup brain samples
+## Nicotine
+rowData(rse_gene)$DE_in_pup_brain_nicotine <- unlist(sapply(rowData(rse_gene)$gencodeID, function(x){if(x %in% de_genes_pups_nicotine_fitted$gencodeID){TRUE} else {FALSE}}))
+## Smoking
+rowData(rse_gene)$DE_in_pup_brain_smoking <- unlist(sapply(rowData(rse_gene)$gencodeID, function(x){if(x %in% de_genes_pups_smoking_fitted$gencodeID){TRUE} else {FALSE}}))
+
+
+########### Exon level ###########
+## (For pup brain samples only)
+
+## Data frames with DE exons
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Exon_analysis/de_exons_nic.Rdata"), verbose = TRUE)
+# de_exons_nic
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Exon_analysis/de_exons_smo.Rdata"), verbose = TRUE)
+# de_exons_smo
+
+## Add info of DE exons to rowData of rse objects
+## DE exons in pup brain samples
+## Nicotine
+rowData(rse_exon)$DE_in_pup_brain_nicotine <- unlist(sapply(rowData(rse_exon)$exon_gencodeID, function(x){if(x %in% de_exons_nic$exon_gencodeID){TRUE} else {FALSE}}))
+## Smoking
+rowData(rse_exon)$DE_in_pup_brain_smoking <- unlist(sapply(rowData(rse_exon)$exon_gencodeID, function(x){if(x %in% de_exons_smo$exon_gencodeID){TRUE} else {FALSE}}))
+
+
+########### Tx level ###########
+## (For pup brain samples only)
+
+## Data frames with DE txs
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Tx_analysis/de_tx_nic.Rdata"), verbose = TRUE)
+# de_tx_nic
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Tx_analysis/de_tx_smo.Rdata"), verbose = TRUE)
+# de_tx_smo
+
+## Add info of DE txs to rowData of rse objects
+## DE txs in pup brain samples
+## Nicotine
+rowData(rse_tx)$DE_in_pup_brain_nicotine <- unlist(sapply(rowData(rse_tx)$transcript_id, function(x){if(x %in% de_tx_nic$transcript_id){TRUE} else {FALSE}}))
+## Smoking
+rowData(rse_tx)$DE_in_pup_brain_smoking <- unlist(sapply(rowData(rse_tx)$transcript_id, function(x){if(x %in% de_tx_smo$transcript_id){TRUE} else {FALSE}}))
+
+
+########### Jxn level ###########
+## (For pup brain samples only)
+
+## Data frames with DE jxns
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Jx_analysis/de_jxns_nic.Rdata"), verbose = TRUE)
+# de_jxns_nic
+load(here("~/Desktop/smokingMouse_Indirects/processed-data/04_DEA/Jx_analysis/de_jxns_smo.Rdata"), verbose = TRUE)
+# de_jxns_smo
+
+## Add info of DE jxns to rowData of rse objects
+## DE jxns in pup brain samples
+## Nicotine
+rowData(rse_jx)$DE_in_pup_brain_nicotine <- unlist(sapply(rownames(rowData(rse_jx)), function(x){if(x %in% rownames(de_jxns_nic)){TRUE} else {FALSE}}))
+## Smoking
+rowData(rse_jx)$DE_in_pup_brain_smoking <- unlist(sapply(rownames(rowData(rse_jx)), function(x){if(x %in% rownames(de_jxns_smo)){TRUE} else {FALSE}}))
+
+
+
+
+
+## Make GRanges from human data
+de_genes_prenatal_human_brain_smoking<- makeGRangesFromDataFrame(fetalGene, keep.extra.columns = TRUE)
+de_genes_adult_human_brain_smoking <- makeGRangesFromDataFrame(adultGene, keep.extra.columns = TRUE)
+
+
+## Add metadata to rse objects
+metadata(rse_gene) <- list(
+  "Obtained_from"="https://github.com/LieberInstitute/smokingMouse_Indirects"
+)
+
+metadata(rse_exon) <- list(
+  "Obtained_from"="https://github.com/LieberInstitute/smokingMouse_Indirects"
+)
+
+metadata(rse_tx) <- list(
+  "Obtained_from"="https://github.com/LieberInstitute/smokingMouse_Indirects"
+)
+
+metadata(rse_jx) <- list(
+  "Obtained_from"="https://github.com/LieberInstitute/smokingMouse_Indirects"
+)
+
+## Add metadata to human data from Semick SA et al. (2018)
+metadata(de_genes_prenatal_human_brain_smoking) <- list(
+  "Downloaded_from"="https://github.com/LieberInstitute/Smoking_DLPFC_Devel",
+  "Cite_this_paper"="https://www.nature.com/articles/s41380-018-0223-1"
+)
+
+metadata(de_genes_adult_human_brain_smoking) <- list(
+  "Downloaded_from"="https://github.com/LieberInstitute/Smoking_DLPFC_Devel",
+  "Cite_this_paper"="https://www.nature.com/articles/s41380-018-0223-1"
+)
+
+
+
+## Save rse objects under more informative names
+save(rse_gene, file="~/Desktop/smokingMouse/inst/extdata/rse_gene_mouse_RNAseq_nic-smo.Rdata", version = 2)
+save(rse_tx, file="~/Desktop/smokingMouse/inst/extdata/rse_tx_mouse_RNAseq_nic-smo.Rdata", version = 2)
+save(rse_jx, file="~/Desktop/smokingMouse/inst/extdata/rse_jx_mouse_RNAseq_nic-smo.Rdata", version = 2)
+save(rse_exon, file="~/Desktop/smokingMouse/inst/extdata/rse_exon_mouse_RNAseq_nic-smo.Rdata", version = 2)
+save(de_genes_prenatal_human_brain_smoking, file="~/Desktop/smokingMouse/inst/extdata/de_genes_prenatal_human_brain_smoking.Rdata", version = 2)
+save(de_genes_adult_human_brain_smoking, file="~/Desktop/smokingMouse/inst/extdata/de_genes_adult_human_brain_smoking.Rdata", version = 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## Load rse objects and human data
 
