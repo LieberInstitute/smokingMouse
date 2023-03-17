c981c405d9420c02b8f37c99cc03b01f214fff48
daianna21 <daianna2121@outlook.es>
2023-03-16 19:05:34 -0600

    Add imports in R/.*R file to avoid warning in BiocCheck()

--- R/smokingMouse-package.R
+++ R/smokingMouse-package.R
@@ -1,7 +1,9 @@
 #' Provides access to data of the smokingMouse project
+#' @import AnnotationHubData
+#' @import ExperimentHub
 #'
-#' @description This is an ExperimentHub package that provides access to the data at the gene, exon, transcript and junction levels,
-#' used in the analyses of the smokingMouse project. See https://github.com/LieberInstitute/smokingMouse_Indirects.
+#' @description This is an ExperimentHub package that provides access to the data at the gene, exon, transcript
+#' and junction levels, used in the analyses of the smokingMouse project. See https://github.com/LieberInstitute/smokingMouse_Indirects.
 #' License: Artistic-2.0
 #' @name smokingMouse-package
 #' @aliases smokingMouse-package smokingMouse
