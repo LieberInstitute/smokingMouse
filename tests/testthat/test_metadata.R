c89b0cfbf79a4b131cbef440430611e7e4d85ad0
daianna21 <daianna2121@outlook.es>
2023-03-22 10:36:09 -0600

    Remove path to pkg

--- tests/testthat/test_metadata.R
+++ tests/testthat/test_metadata.R
@@ -2,6 +2,5 @@ context("metadata validity")
 
 test_that("metadata is valid",
 {
-    pathToPackage = "/Users/daiannagonzalez/Desktop/smokingMouse"
-    expect_type(AnnotationHubData::makeAnnotationHubMetadata(pathToPackage , "metadata.csv"), "list")
+    expect_type(AnnotationHubData::makeAnnotationHubMetadata("metadata.csv"), "list")
 })
