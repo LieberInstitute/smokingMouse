cc074123e4268de7817039f0b955e2441336f88f
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-06-09 17:45:20 -0400

    v0.99.3 -- don't import AnnotationHubData and ExperimentHub packages anymore

--- R/smokingMouse-package.R
+++ R/smokingMouse-package.R
@@ -1,6 +1,4 @@
 #' Provides access to data of the smokingMouse project
-#' @import AnnotationHubData
-#' @import ExperimentHub
 #'
 #' @description This is an ExperimentHub package that provides access to the data at the gene, exon, transcript and junction level, used
 #' in the analyses of the smokingMouse project. See https://github.com/LieberInstitute/smokingMouse_Indirects.
